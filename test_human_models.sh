d9cb2396f1fbc2180c6e0fba2659e815ac4e4fe4
Devlin-Moyer <devmoy@gmail.com>
2024-04-29 18:09:23 -0400

    feat: fixed requirements.txt file, added in data and figure files, added .gitignore

---
+++ test_human_models.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#$ -m e
+#$ -j y
+#$ -l h_rt=24:00:00
+#$ -pe omp 16
+#$ -t 1-2
+
+source bin/activate
+
+if [ $SGE_TASK_ID == 1 ] 
+then
+  python run_tests_on_human.py 1.15 8 > figure_data/Human-GEMv1.15_log.txt
+else
+  python run_tests_on_human.py 1.18 8 > figure_data/Human-GEMv1.18_log.txt
+fi
